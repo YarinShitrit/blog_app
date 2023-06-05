3461efd581191cd89b618b66f4810ce72f9c1efd
YarinShitrit <yarinshitrit@gmail.com>
2023-06-06 01:27:54 +0300

    added blog update

--- controllers/blogsController.js
+++ controllers/blogsController.js
@@ -1,7 +1,7 @@
 const Blog = require('../models/blog')
 
 const blogs_index = (req, res) => {
-    Blog.find().sort({createdAt: -1})
+    Blog.find().sort({updatedAt: -1})
     .then((result) => {
         res.render('blogs/index', {title: 'All Blogs', blogs: result})
     })
@@ -35,6 +35,27 @@ const blog_create_post = (req, res) => {
         })
 }
 
+const blog_update_get = (req, res) => {
+    Blog.findById(req.params.id)
+        .then((blog) => {
+            res.render('blogs/update', {blog: blog, title: 'Update blog'})
+        })
+        .catch((err) => {
+            res.status(404).render('404', {title: "Blog not found"})
+        })
+}
+
+const blog_update = (req, res) => {
+    console.log(req.body)
+    Blog.findByIdAndUpdate(req.params.id, req.body)
+    .then((result) => {
+        res.redirect('/blogs');
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
 const blog_delete = (req, res) => {
     const id = req.params.id
     Blog.findByIdAndDelete(id)
@@ -51,5 +72,8 @@ module.exports = {
     blog_details,
     blog_create_get,
     blog_create_post,
+    blog_update_get,
+    blog_update,
     blog_delete
+    
 }
--- routes/blogsRoute.js
+++ routes/blogsRoute.js
@@ -5,8 +5,10 @@ const router = express.Router();
 
 router.get('/', blogsController.blogs_index)
 router.post('/', blogsController.blog_create_post)
+router.post('/update/:id', blogsController.blog_update)
 router.get('/create', blogsController.blog_create_get)
 router.get('/:id', blogsController.blog_details)
+router.get('/update/:id', blogsController.blog_update_get)
 router.delete('/:id', blogsController.blog_delete)
 
 module.exports = router;
