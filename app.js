8f1418447fadb0b9758f06d39535b936543f692c
YarinShitrit <yarinshitrit@gmail.com>
2023-06-05 18:22:42 +0300

    adjusted to MVC

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const blogRoutes = require('./routes/blogsRoute')
 
 // express app
 const app = express()
 
-//register ejs as view engine
+// connect to MongoDB
+const dbURI = 'mongodb+srv://admin:admin@blogs.n0nzihu.mongodb.net/db?retryWrites=true&w=majority'
+mongoose.connect(dbURI)
+    .then((result) => app.listen(8080))
+    .catch((err) => console.log(err))
+
+// register ejs as view engine
 app.set('view engine', 'ejs')
 
-// listen for requests
-app.listen(8080)
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({extended : true}))
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+  });
 
-app.get('/', (req, res) => {
-    const blogs = [{"title": 1, "snippet": "test1"}]
-    res.render('index', {title: 'Home', blogs})
+app.get('/', (req, res) => {   
+    res.redirect('/blogs')
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {title: 'About'})
 })
 
-// redirects
-app.get('/blogs/create', (req, res) => {
-    res.render('create', {title: 'Create blog'})
-})
+// blogs routes
+app.use('/blogs', blogRoutes)
 
-//404 page
+
+// 404 page
 app.use((req, res) =>{
     res.render('404', {title: '404'})
 })
